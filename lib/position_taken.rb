261c894f1a82013dc180aa011a89f66b85d05ed4
Jeanette Payne <jeanetterpayne@gmail.com>
2020-06-30 01:01:50 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -6,7 +6,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def position_taken?(board, index)
   if board[0] == " " || "" || nil
     FALSE
-  elsif board[0] 
+  elsif board[0] != " " || "" || nil
     TRUE
   end
 end
